8601387e9859a6dad3e439f35512d6d91d455de7
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-12 13:33:38 -0300

    feat: adicão da classe math e a classe de tratamento de exceptions

---
+++ src/main/java/br/com/monteiro/exception/ExceptionResponse.java
@@ -0,0 +1,2 @@
+package br.com.monteiro.exception;public class ExceptionResponse {
+}
---
+++ src/main/java/br/com/monteiro/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,4 @@
+package br.com.monteiro.exception.handler;
+
+public class CustomizedResponseEntityHandler {
+}
