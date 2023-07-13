86d30f70a5aedd13672a5ab1947279751e345198
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-13 00:08:06 -0300

    feat: add create, update and delete person

--- src/main/java/br/com/monteiro/controller/PersonController.java
+++ src/main/java/br/com/monteiro/controller/PersonController.java
@@ -4,10 +4,9 @@ import br.com.monteiro.model.Person;
 import br.com.monteiro.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -15,12 +14,37 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
+    @RequestMapping(value = "/", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    private Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    private Person update(@RequestBody Person person) {
+        return service.create(person);
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    private Person findById(@PathVariable(value = "id") String id) throws Exception {
+    private Person findById(@PathVariable(value = "id") String id) {
         return service.findById(id);
     }
 
+    @RequestMapping(value = "/", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    private List<Person> findAll() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    private void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+    }
 
 }
 
