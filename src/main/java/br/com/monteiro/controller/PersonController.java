0765eadba6d07caa9e116f039660ce1c85de540c
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-17 09:24:55 -0300

    refactor: alteracao metodos requestmapping no person controller

--- src/main/java/br/com/monteiro/controller/PersonController.java
+++ src/main/java/br/com/monteiro/controller/PersonController.java
@@ -4,6 +4,7 @@ import br.com.monteiro.model.Person;
 import br.com.monteiro.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,36 +15,35 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST,
+    @PostMapping(value = "/",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    private Person create(@RequestBody Person person) {
+    public Person create(@RequestBody Person person) {
         return service.create(person);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.PUT,
+    @PutMapping(value = "/",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    private Person update(@RequestBody Person person) {
+    public Person update(@RequestBody Person person) {
         return service.create(person);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    private Person findById(@PathVariable(value = "id") Long id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    private List<Person> findAll() {
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE,
+    @DeleteMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    private void delete(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
