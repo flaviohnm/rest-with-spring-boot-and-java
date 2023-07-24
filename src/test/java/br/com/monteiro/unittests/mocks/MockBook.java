eb1b15693bc2c06ec599147913554224da9aca3c
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-24 13:48:49 -0300

    fix: correcao de nomes para todos os testes funcionarem corretamente

---
+++ src/test/java/br/com/monteiro/unittests/mocks/MockBook.java
@@ -0,0 +1,57 @@
+package br.com.monteiro.unittests.mocks;
+
+import br.com.monteiro.data.vo.v1.BookVO;
+import br.com.monteiro.model.Book;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MockBook {
+
+
+    public Book mockEntity() {
+        return mockEntity(1);
+    }
+
+    public BookVO mockVO() {
+        return mockVO(1);
+    }
+
+    public List<Book> mockEntityList() {
+        List<Book> persons = new ArrayList<Book>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<BookVO> mockVOList() {
+        List<BookVO> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockVO(i));
+        }
+        return persons;
+    }
+
+    public Book mockEntity(Integer number) {
+        Book book = new Book();
+        book.setId(number.longValue());
+        book.setAuhtor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(25D);
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+    public BookVO mockVO(Integer number) {
+        BookVO book = new BookVO();
+        book.setKey(number.longValue());
+        book.setAuhtor("Some Author" + number);
+        book.setLaunchDate(new Date());
+        book.setPrice(25D);
+        book.setTitle("Some Title" + number);
+        return book;
+    }
+
+}
