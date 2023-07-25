c5faf5bcaa4080ec444cd0b8771af98c9de06201
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-25 14:29:44 -0300

    feat: add dependency to testcontainers

---
+++ src/test/java/br/com/monteiro/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,2 @@
+package br.com.monteiro.integrationtests.testcontainers;public class AbstractIntegrationTest {
+}
---
+++ src/test/java/br/com/monteiro/unittests/configs/TestConfigs.java
@@ -0,0 +1,2 @@
+package br.com.monteiro.unittests.configs;public class TestConfigs {
+}
