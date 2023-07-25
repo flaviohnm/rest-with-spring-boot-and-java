4d747223014dde9d1b0111c9da311e5e6a14209a
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-25 14:30:59 -0300

    feat: add class to execute integration tests

--- src/test/java/br/com/monteiro/unittests/configs/TestConfigs.java
+++ src/test/java/br/com/monteiro/unittests/configs/TestConfigs.java
@@ -1,2 +1,7 @@
-package br.com.monteiro.unittests.configs;public class TestConfigs {
+package br.com.monteiro.unittests.configs;
+
+public class TestConfigs {
+
+    public static final int  SERVER_PORT = 8888;
+
 }
