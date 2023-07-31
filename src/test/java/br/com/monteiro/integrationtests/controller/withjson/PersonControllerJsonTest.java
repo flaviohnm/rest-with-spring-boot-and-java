ccc0585049c3cf60f53a67555e4f36ef61ce49a1
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-31 08:52:20 -0300

    feat: add integration test to post PersonController

--- src/test/java/br/com/monteiro/integrationtests/controller/withjson/PersonControllerJsonTest.java
+++ src/test/java/br/com/monteiro/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -1,30 +1,90 @@
-package br.com.monteiro.integrationtests.swagger;
+package br.com.monteiro.integrationtests.controller.withjson;
 
+import br.com.monteiro.data.vo.v1.PersonVO;
 import br.com.monteiro.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.monteiro.unittests.configs.TestConfigs;
-import org.junit.jupiter.api.Test;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class PersonControllerJsonTest extends AbstractIntegrationTest{
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class PersonControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+    private static PersonVO person;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        person = new PersonVO();
+
+    }
 
     @Test
-    public void shouldDisplaySwaggerUiPage() {
-        var content =
-                given()
-                        .basePath("/swagger-ui/index.html")
-                        .port(TestConfigs.SERVER_PORT)
-                        .when()
-                        .get()
-                        .then()
-                        .statusCode(200)
-                        .extract()
-                        .body()
-                        .asString();
-        assertTrue(content.contains("Swagger UI"));
+    @Order(1)
+    public void testCreate() throws IOException {
+        mockPerson();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADAER_PARAM_ORIGIN, "http://monteiro.com.br")
+                .setBasePath("/api/person/v1/")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        PersonVO createdPerson = objectMapper.readValue(content, PersonVO.class);
+        person = createdPerson;
+
+        assertNotNull(createdPerson);
+        assertNotNull(createdPerson.getKey());
+        assertNotNull(createdPerson.getFirstName());
+        assertNotNull(createdPerson.getLastName());
+        assertNotNull(createdPerson.getAddress());
+        assertNotNull(createdPerson.getGender());
+
+        assertTrue(createdPerson.getKey() > 0);
+
+        assertEquals("Richard", createdPerson.getFirstName());
+        assertEquals("Stallman", createdPerson.getLastName());
+        assertEquals("New York City, New York, US", createdPerson.getAddress());
+        assertEquals("Male", createdPerson.getGender());
+    }
+
+    private void mockPerson() {
+        person.setFirstName("Richard");
+        person.setLastName("Stallman");
+        person.setAddress("New York City, New York, US");
+        person.setGender("Male");
     }
 
 }
--- src/test/java/br/com/monteiro/integrationtests/vo/PersonVO.java
+++ src/test/java/br/com/monteiro/integrationtests/vo/PersonVO.java
@@ -1,19 +1,12 @@
-package br.com.monteiro.data.vo.v1;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.github.dozermapper.core.Mapping;
-import org.springframework.hateoas.RepresentationModel;
+package br.com.monteiro.integrationtests.vo;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-@JsonPropertyOrder({"id","firstName","lastName","address","gender"})
-public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
+public class PersonVO implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @JsonProperty("id")
-    @Mapping("id")
-    private Long key;
+    private Long id;
     private String firstName;
     private String lastName;
     private String address;
@@ -22,12 +15,20 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
     public PersonVO() {
     }
 
-    public Long getKey() {
-        return key;
+    public PersonVO(Long id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
     }
 
-    public void setKey(Long key) {
-        this.key = key;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -60,5 +61,18 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
 
     public void setGender(String gender) {
         this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonVO personVO = (PersonVO) o;
+        return Objects.equals(id, personVO.id) && Objects.equals(firstName, personVO.firstName) && Objects.equals(lastName, personVO.lastName) && Objects.equals(address, personVO.address) && Objects.equals(gender, personVO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
     }
 }
