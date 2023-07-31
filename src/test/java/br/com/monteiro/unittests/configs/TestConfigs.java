082baf6f641ab3579c3ebf8027312fb2b3a624f6
Flavio Monteiro <flaviohnm@gmail.com>
2023-07-31 08:51:26 -0300

    feat: add TestConfig to IntegrationTest

--- src/test/java/br/com/monteiro/unittests/configs/TestConfigs.java
+++ src/test/java/br/com/monteiro/unittests/configs/TestConfigs.java
@@ -4,4 +4,12 @@ public class TestConfigs {
 
     public static final int  SERVER_PORT = 8888;
 
+    public static final String HEADAER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String HEADAER_PARAM_ORIGIN = "Origin";
+
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_YML = "application/x-yaml";
+
+
 }
