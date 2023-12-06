f147732a40591270514a18bc0fc2897fdf92665a
Flavio Monteiro <flaviohnm@gmail.com>
2023-12-06 15:40:22 -0300

    feat: update migration user insert password

--- src/main/resources/db/migration/V9__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V9__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '2b6f0f8cf263250df92a4f8381f5e97efcc91f9951f8c0f9d2eb1c9a893f1775e8c634e36fffe27f', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '6d65c251350a306df9c6c8803f2fefc1c3fee78159133c68b7304bd6af95161a7ce312607ebf142b', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro Costa', 'f6d634f9b773f9a42e44c66ae62afdf513589b2d1b3d4802814e421373cdc3e98529ed62c371dda9', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '324421d3c218eee6598a8beb43219b39e3371ef6c86ca9eb8a3a4101af146a860f12371a1833d1a2', b'1', b'1', b'1', b'1');
