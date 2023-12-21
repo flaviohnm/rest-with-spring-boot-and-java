7a9911908610cfdf2babd56750b03e7c39853f67
Flavio Monteiro <flaviohnm@gmail.com>
2023-12-21 09:05:29 -0300

    feat: implementation of paged search in findAll

--- src/main/java/br/com/monteiro/service/PersonServices.java
+++ src/main/java/br/com/monteiro/service/PersonServices.java
@@ -9,9 +9,12 @@ import br.com.monteiro.model.Person;
 import br.com.monteiro.repositories.PersonRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.logging.Logger;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -23,15 +26,19 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<PersonVO> findAll() {
+    public Page<PersonVO> findAll(Pageable pageable) {
 
         logger.info("Finding all people!");
 
-        var persons = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
-        persons.forEach(p -> p.add(linkTo(methodOn(PersonController.class)
-                .findById(p.getKey()))
-                .withSelfRel()));
-        return persons;
+        var personPage = repository.findAll(pageable);
+
+        var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+        personVosPage.map(
+                p -> p.add(
+                        linkTo(methodOn(PersonController.class)
+                                .findById(p.getKey()))
+                                .withSelfRel()));
+        return personVosPage;
     }
 
     public PersonVO findById(Long id) {
